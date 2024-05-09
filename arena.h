ae3742a53e9371f2563b341b1ef98dd4dc44eb19
xetri <xetrymebich@gmail.com>
2024-05-09 17:49:20 +0545

    wrapper .h

---
+++ arena.h
@@ -0,0 +1,15 @@
+#ifndef _ARENA_
+#define _ARENA_
+
+#ifdef ARENA_USE
+#define ARENA_U8_USE
+#define ARENA_STRING_USE
+#define ARENA_ARRAY_USE
+#endif
+
+#include "./basic.h"
+#include "./u8.h"
+#include "./string.h"
+#include "./array.h"
+
+#endif
